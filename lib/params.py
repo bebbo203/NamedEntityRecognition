4157c3ba589ca9b7127884c20be4dd11745c3f75
Roberto Aureli <bebbo203@gmail.com>
2020-04-20 20:00:46 +0200

    change

--- lib/params.py
+++ lib/params.py
@@ -1,16 +1,16 @@
 class Params():
-    hidden_dim = 128
+    hidden_dim = 64
     embedding_dim = 50
     bidirectional = False
-    num_layers = 1
-    dropout = 0.3
+    num_layers = 3
+    dropout = 0.5
     embeddings_processed_weights = 'model/embeddings_weights.json'
     embeddings_path = 'model/glove.6B.50d.txt'
-    #embeddings_path = None
+    
     device = "cuda"
-    window_size = 100
-    window_shift = 100
-    min_freq = 2
+    window_size = 5
+    window_shift = 1
+    min_freq = 5
     max_freq = 0
     vocabulary_path = 'model/vocabulary.json'
     label_vocabulary_path = 'model/label_vocabulary.json'
