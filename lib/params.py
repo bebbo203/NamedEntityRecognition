557221b137780a3488eeb982af21c2a8170e624b
Roberto Aureli <bebbo203@gmail.com>
2020-04-24 20:21:49 +0200

    chars

--- lib/params.py
+++ lib/params.py
@@ -5,13 +5,13 @@ class Params():
     single_char_embedding_dim = 4
     alphabet_size = 107
     bidirectional = True
-    num_layers = 4
+    num_layers = 2
     dropout = 0.3
     embeddings_processed_weights = 'model/embeddings_weights.json'
     embeddings_path = 'model/glove.6B.50d.txt'
     max_word_lenght = 20
     device = "cuda"
-    window_size = 5
+    window_size = 10
     window_shift =  1
     min_freq = 2
     max_freq = 0
