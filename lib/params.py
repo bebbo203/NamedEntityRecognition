a9d413ff8fe7c56561af1b647dc6c474cd243e34
Roberto Aureli <bebbo203@gmail.com>
2020-04-24 15:47:44 +0200

    m

--- lib/params.py
+++ lib/params.py
@@ -1,19 +1,19 @@
 class Params():
-    hidden_dim = 512
+    hidden_dim = 256
     embedding_dim = 50
     char_embedding_dim = 10
     single_char_embedding_dim = 4
     alphabet_size = 107
     bidirectional = True
-    num_layers = 2
-    dropout = 0.5
+    num_layers = 4
+    dropout = 0.3
     embeddings_processed_weights = 'model/embeddings_weights.json'
     embeddings_path = 'model/glove.6B.50d.txt'
-    max_word_lenght = 16
+    max_word_lenght = 20
     device = "cuda"
     window_size = 5
-    window_shift =  5
-    min_freq = 0
+    window_shift =  1
+    min_freq = 2
     max_freq = 0
     vocabulary_path = 'model/vocabulary.json'
     label_vocabulary_path = 'model/label_vocabulary.json'
